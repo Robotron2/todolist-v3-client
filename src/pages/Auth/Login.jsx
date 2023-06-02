9f68c504d41f1c2a6d9a5b545a3173c692934706
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-06-02 19:20:13 +0100

    Login successfully

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,39 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation, useNavigate } from "react-router-dom"
 import Layout from "../../components/Layout/Layout"
 import SVG from "../../components/Layout/SVG"
+import { useState } from "react"
+import useAuth from "./../../components/CustomHooks/useAuth"
+import axios from "axios"
+import { toast } from "react-hot-toast"
 
 const Login = () => {
-	const handleSubmit = (e) => {}
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const navigate = useNavigate()
+	const location = useLocation()
+	const [auth, setAuth] = useAuth()
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const res = await axios.post(`${import.meta.env.VITE_REACT_APP_API}/api/v1/auth/login`, { email, password })
+			if (res && res.data.success) {
+				toast.success(res && res.data.message)
+				setAuth({
+					...auth,
+					user: res.data.user,
+					token: res.data.token
+				})
+				localStorage.setItem("auth", JSON.stringify(res.data))
+				navigate(location.state || `/`)
+			} else {
+				toast.error(res.data.message)
+			}
+		} catch (error) {
+			console.log(error)
+			toast.error("Something went wrong, please try again")
+		}
+	}
 
 	return (
 		<>
@@ -14,11 +44,31 @@ const Login = () => {
 						<form action="/login" method="post" onSubmit={handleSubmit}>
 							<div className="field">
 								{/* <span className="bx bxs-envelope" /> */}
-								<input type="email" placeholder="Email" name="useremail" required autoComplete="off" />
+								<input
+									type="email"
+									placeholder="Email"
+									name="useremail"
+									required
+									autoComplete="off"
+									onChange={(e) => {
+										setEmail(e.target.value)
+									}}
+									value={email}
+								/>
 							</div>
 							<div className="field">
 								{/* <span className="bx bxs-lock-alt" /> */}
-								<input type="password" placeholder="Password" name="password" required autoComplete="off" />
+								<input
+									type="password"
+									placeholder="Password"
+									name="password"
+									required
+									autoComplete="off"
+									onChange={(e) => {
+										setPassword(e.target.value)
+									}}
+									value={password}
+								/>
 							</div>
 							<div>
 								<button type="submit" className="login-button btn btn-wine">
