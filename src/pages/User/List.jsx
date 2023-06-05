031b10d2609abc4b33f2b732516cfd5162480b2b
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-06-05 15:26:51 +0100

    Display all list items conditionally.

--- src/pages/User/List.jsx
+++ src/pages/User/List.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios"
 import Layout from "../../components/Layout/Layout"
 import SVG from "./../../components/Layout/SVG"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { toast } from "react-hot-toast"
 
 const List = () => {
@@ -9,7 +10,10 @@ const List = () => {
 	const userProfile = userDetails.user
 	const userName = userProfile.name
 	const userId = userProfile._id
+
 	const [newItem, setNewItem] = useState("")
+	const [adder, setAdder] = useState(false)
+	const [lists, setList] = useState([])
 
 	// {_id: '647a31d8193e642bd8ce91e6', name: 'user', email: 'user@user.com'}
 
@@ -36,14 +40,27 @@ const List = () => {
 	// 	}
 	// }
 
+	useEffect(() => {
+		async function fetchAllList() {
+			let res = await axios.get(`${import.meta.env.VITE_REACT_APP_API}/api/v1/user/${userId}`)
+			console.log(res.data.todoArray)
+			setList(res.data.todoArray)
+			console.log(lists)
+		}
+		fetchAllList()
+	}, [adder])
+
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 
 		try {
 			const res = await axios.post(`${import.meta.env.VITE_REACT_APP_API}/api/v1/user/list`, { newItem, userId })
 			console.log(res)
+
 			if (res && res.data) {
 				toast.success("list updated!")
+				setAdder(true)
+				setNewItem("")
 			} else {
 				toast.error(res.data.message)
 			}
@@ -73,6 +90,7 @@ const List = () => {
 												autoComplete="off"
 												onChange={(e) => {
 													setNewItem(e.target.value)
+													setAdder(false)
 												}}
 												value={newItem}
 											/>
@@ -84,15 +102,45 @@ const List = () => {
 									</form>
 
 									<div className="todo-list">
-										<form action="/delete" method="post">
-											<div className="checker">
-												<span className="col-lg-6 col-md-3 col-sm-2">
-													<input type="checkbox" name="checkbox" />
-													<input type="hidden" name="userId" />
-												</span>
-											</div>
-											<span>Hello</span>
-										</form>
+										{lists &&
+											lists.map((list) => {
+												return (
+													<>
+														<div key="1" className="my-1">
+															<form action="/delete" method="post">
+																<div className="checker">
+																	<span className="col-lg-6 col-md-3 col-sm-2">
+																		<input type="checkbox" name="checkbox" />
+																		<input type="hidden" name="userId" />
+																	</span>
+																</div>
+																<span className="mx-4 todoItem">{list.name}</span>
+															</form>
+														</div>
+													</>
+												)
+											})}
+
+										<div className="todo-list">
+											{!lists && (
+												<>
+													<h4 className="text-center purple">Empty list. Add new items.</h4>
+												</>
+											)}
+										</div>
+										{/* {lists.forEach((list) => {
+											return (
+												<form action="/delete" method="post">
+													<div className="checker">
+														<span className="col-lg-6 col-md-3 col-sm-2">
+															<input type="checkbox" name="checkbox" />
+															<input type="hidden" name="userId" />
+														</span>
+													</div>
+													<span>{list.name}</span>
+												</form>
+											)
+										})} */}
 									</div>
 								</div>
 							</div>
